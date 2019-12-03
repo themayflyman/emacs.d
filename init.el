93c8323260d81e8faf672f2f2d2deb570df48cfe
themaflyman <xuziwei100@gmail.com>
2019-12-03 20:12:10 +0800

    install vterm from MELPA

--- init.el
+++ init.el
@@ -15,8 +15,8 @@
 (add-to-list 'default-frame-alist
              '(font . "courier"))
 
-(add-to-list 'load-path "~/.emacs.d/emacs-libvterm")
-(require 'vterm)
+; (add-to-list 'load-path "~/.emacs.d/emacs-libvterm")
+; (require 'vterm)
 
 (require 'whitespace)
 (setq whitespace-line-column 80) ;; limit line length
@@ -24,15 +24,32 @@
 
 (add-hook 'prog-mode-hook 'whitespace-mode)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company vterm))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package vterm
+    :ensure t
+)
